c3d8967ca3bbff7294e8859cf007f19008a7f341
Marcus V P Cavalcanti <vincius.cavalcanti16@gmail.com>
2021-04-10 16:05:22 -0300

    refatoração de RecognizerDTO

--- src/test/java/br/com/tsi/utfpr/xenon/XenonApplicationTests.java
+++ src/test/java/br/com/tsi/utfpr/xenon/XenonApplicationTests.java
@@ -2,7 +2,9 @@ package br.com.tsi.utfpr.xenon;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class XenonApplicationTests {
 
