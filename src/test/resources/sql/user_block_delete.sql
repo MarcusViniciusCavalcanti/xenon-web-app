951131df3cf5655ab232df25e4301e3844a00394
Marcus V P Cavalcanti <vincius.cavalcanti16@gmail.com>
2021-04-11 13:17:36 -0300

    adicionado mensagens de erro customizadas

---
+++ src/test/resources/sql/user_block_delete.sql
@@ -0,0 +1,5 @@
+delete from access_card_has_roles where access_card_id in('155', '156', '157', '158');
+delete from users where access_card_id in('155', '156', '157', '158');
+delete from access_card where id in('155', '156', '157', '158');;
+
+
